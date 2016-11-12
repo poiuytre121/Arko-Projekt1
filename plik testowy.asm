1afff5e6a943abf17e7373739b6417d291251326
Przemysław Rogalski <p.rogalski@o2.pl>
2016-11-12 01:08:17 +0100

    Found End key code

--- plik testowy.asm
+++ plik testowy.asm
@@ -1,16 +1,27 @@
 section .text
 org 100h
 
-mov ah,1
+mov ah,8 ;pobiera znak z klawiatury bez echa
 int 21h
 
-add al,"0"
-mov dl,al
 
-mov ah,2
+cmp al,0 ;sprawdza, czy znak pochodzi z rodziny dwuczęściowych kodów
+jne dalej
+mov ah,8
 int 21h
+cmp al,79 ;sprawdza czy jest to end, dla home będzie 71
+jne dalej
+mov dx,napis ;wyświetla napis, jeżeli jest to zadany przycisk
+mov ah,9
+int 21h
+dalej:
+
 mov ax,4C00h
 int 21h
 ;wnioski:
 ;kod tabulatora = 9
 ;kod entera = 13
+;kod end = (0;79)
+
+section .data
+napis db "Weszlo$"
